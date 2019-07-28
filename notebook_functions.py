39b56f851de3223e99903ef7ff7d47adf90440a8
Pablo S <dr_pablo@Pablos-MacBook-Air.local>
2019-07-28 17:19:50 -0400

    renamed files

--- notebook_functions.py
+++ notebook_functions.py
@@ -5,7 +5,11 @@ import numpy as np
 
 
 def meld_score(x, y, z):
-	"""function to compute patient MELD scores; MELD score formula uses Total Bili, INR, and Creatinine"""
+	
+	"""
+	function to compute patient MELD scores; MELD score formula uses Total Bili, INR, and Creatinine
+
+	"""
     return (((np.log(x)*0.378) + (np.log(y)*1.120)  + (np.log(z)*0.957)+0.643)*10)
 
  
